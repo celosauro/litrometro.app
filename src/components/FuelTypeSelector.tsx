df30f4445428cd9415578b0c9a71eb9318a6c8e9
Marcelo de Andrade <andrade.asmarcelo@gmail.com>
2026-04-09 15:06:42 -0300

    Fix

--- src/components/FuelTypeSelector.tsx
+++ src/components/FuelTypeSelector.tsx
@@ -9,30 +9,28 @@ export function SeletorTipoCombustivel({ selecionado, aoMudar }: SeletorTipoComb
   const tiposCombustivel = Object.entries(TIPOS_COMBUSTIVEL) as [string, string][];
 
   return (
-    <div className="-mx-4 px-4 overflow-x-auto scrollbar-hide">
-      <div className="flex gap-2 pb-1">
-        {tiposCombustivel.map(([tipoStr, nome]) => {
-          const tipo = Number(tipoStr) as TipoCombustivel;
-          const estaSelecionado = tipo === selecionado;
-          const classeCor = CORES_COMBUSTIVEL[tipo];
+    <div className="flex flex-wrap gap-2">
+      {tiposCombustivel.map(([tipoStr, nome]) => {
+        const tipo = Number(tipoStr) as TipoCombustivel;
+        const estaSelecionado = tipo === selecionado;
+        const classeCor = CORES_COMBUSTIVEL[tipo];
 
-          return (
-            <button
-              key={tipo}
-              onClick={() => aoMudar(tipo)}
-              className={`
-                fuel-type-btn
-                ${estaSelecionado
-                  ? `fuel-type-btn-active ${classeCor}`
-                  : 'fuel-type-btn-inactive'
-                }
-              `}
-            >
-              {nome}
-            </button>
-          );
-        })}
-      </div>
+        return (
+          <button
+            key={tipo}
+            onClick={() => aoMudar(tipo)}
+            className={`
+              fuel-type-btn
+              ${estaSelecionado
+                ? `fuel-type-btn-active ${classeCor}`
+                : 'fuel-type-btn-inactive'
+              }
+            `}
+          >
+            {nome}
+          </button>
+        );
+      })}
     </div>
   );
 }
